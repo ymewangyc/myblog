66feb52f74ef1621768dc848b9917c881b798abb
x318 <x318@example.com>
2018-02-01 11:37:09 +0800

    gsMarkdown使用

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -15,13 +15,40 @@ def index(request):
         'welcome':'欢迎访问我的博客首页'
     })
 '''
+import markdown
 from django.shortcuts import render,get_object_or_404
 from .models import Post
+
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
+'''
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    # 记得在顶部引入 markdown 模块
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                     'markdown.extensions.extra',
+                                     'markdown.extensions.codehilite',
+                                     'markdown.extensions.toc',
+                                  ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+'''
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    return render(request, 'blog/detail.html', context={'post': post})
+
+
+# def detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'blog/detail.html', context={'post': post})
